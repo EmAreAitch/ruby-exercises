0c4a2f4ade56a21b461c4f249e667fb4fe763ad5
EmAreAitch <rahibhasan95@gmail.com>
2023-03-14 21:12:22 +0530

    Methods exercise completed

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -11,7 +11,7 @@ def favorite_color(favorite_list)
 end
 
 def favorite_number(favorite_list)
-  favorite_list.fetch(:number,42)
+  favorite_list.fetch :number,42
   # use #fetch to return the value of the number key or 42 if the key is not found
 end
 
@@ -25,7 +25,7 @@ end
 
 def remove_favorite_number(favorite_list)
   # Step 1: delete the number data
-  favorite_list.delete(:number)
+  favorite_list.delete :number
   # Step 2: return the hash (because Step 1 returns the value of the number key)
   favorite_list
 end
@@ -41,6 +41,6 @@ def favorite_items(favorite_list)
 end
 
 def merge_favorites(original_list, additional_list)
-  original_list.merge(additional_list)
+  original_list.merge additional_list
   # merge the two hashes: original_list and additional_list
 end
